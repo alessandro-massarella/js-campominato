ffb2eb3ce8e0c7f16ef348af658a5713b3ea83df
alessandro <alessandro.massarella@gmail.com>
2020-10-28 18:35:05 +0100

    Step 2

--- js/script.js
+++ js/script.js
@@ -25,16 +25,42 @@ return numRandom;
 //     console.log('numero', numero);
 // }
 // });
-
+document.getElementById("myButton").addEventListener("click", function() {
 // creo un array che conterrà i 16 numeri
-var numbers = [];
+var numbersArray = [];
 
 // CON UN CICLO WHILE GENERO I NUMERI CASUALI NON RIPETUTI
 var i = 0;
 while (i < 16) {
   var numRandom = numeroRandom(1, 100);
-  if (!numbers.includes(numRandom));
-  numbers.push(numRandom);
-  i++
+  if (!numbersArray.includes(numRandom));
+      numbersArray.push(numRandom);
+  i++;
 }
-console.log('numeri', numbers);
+console.log('numeri', numbersArray);
+
+// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L'utente non può inserire più volte lo stesso numero.
+
+// CHIEDO ALL'UTENTE DI INSERIRE I numeri per N VOLTE, CREANDO UN CICLO
+for (var i = 0; i < 5; i++) {
+      var numeroUser = parseInt(prompt('inserisci un numero'));
+      if (!numbersArray.includes(numeroUser)) {
+       numbersArray.push(numeroUser);
+     } else {
+       alert('hai perso');
+       alert('punteggio: ' + i);
+       break;
+     }
+}
+
+console.log('array', numbersArray);
+// var i = 0;
+// while (i < 5) {
+//     var numeroUser = parseInt(prompt('inserisci un numero'));
+//     if (!numbersArray.includes(numeroUser))
+//           numbersArray.push(numeroUser);
+//     i++;
+// }
+// console.log('userNumbers', userNumbers);
+});
